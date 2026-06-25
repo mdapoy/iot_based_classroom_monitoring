833541fdf409aa316b610c9d43f6b4aad42b5b3c
mdapoy <dafi.fat2004@gmail.com>
2026-06-26 00:18:56 +0700

    update HSTS

--- main.py
+++ main.py
@@ -76,11 +76,12 @@ class MaxBodySizeMiddleware(BaseHTTPMiddleware):
 class SecurityHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
-        response.headers["X-Content-Type-Options"]  = "nosniff"
-        response.headers["X-Frame-Options"]          = "DENY"
-        response.headers["X-XSS-Protection"]         = "1; mode=block"
-        response.headers["Referrer-Policy"]           = "strict-origin-when-cross-origin"
-        response.headers["Permissions-Policy"]        = "geolocation=(), microphone=(), camera=()"
+        response.headers["X-Content-Type-Options"]       = "nosniff"
+        response.headers["X-Frame-Options"]            = "DENY"
+        response.headers["X-XSS-Protection"]           = "1; mode=block"
+        response.headers["Referrer-Policy"]             = "strict-origin-when-cross-origin"
+        response.headers["Permissions-Policy"]          = "geolocation=(), microphone=(), camera=()"
+        response.headers["Strict-Transport-Security"]   = "max-age=31536000; includeSubDomains"
         return response
 
 
